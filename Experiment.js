8d2213b71c9b9f22d95c2d806bafeb22b37d72ce
Grey Johnson <grey.johnson@ufl.edu>
2021-01-19 22:29:32 -0500

    Local Dev Debugging

--- Experiment.js
+++ Experiment.js
@@ -1,10 +1,9 @@
 function Experiment(jsSheetHandle, jsPsychHandle, codes) {
-    jsSheetHandle.CreateSession(RunExperiment)
+    jsSheetHandle.CreateSession(RunExperiment);
 
     function RunExperiment(session) {
         // Define Constants
-        const CONTACT_EMAIL = 'fake@email.com'
-        const CREDIT_URL = `<CREDIT_URL>&survey_code=${codes.survey_code}`
+        const CREDIT_URL = `<CREDIT_URL>&survey_code=${codes.survey_code}`;
 
         // Define Experiment Trials
         let welcomeTrial = {
@@ -13,18 +12,122 @@ function Experiment(jsSheetHandle, jsPsychHandle, codes) {
                 <p>Welcome to the experiment.</p>
                 <p>Press any key to begin.</p>
             `
+        };
+
+        let checkVisionTrial = {
+            type: 'survey-multi-choice',
+            questions: [{
+                name: 'vision',
+                prompt: 'Do you have normal or correct-to-normal vision?',
+                options: ['Normal', 'Corrected-to-Normal', 'Other'],
+                required: true
+            }]
+        };
+
+        let consentFormTrial = {
+            type: 'external-html',
+            url: 'https://ufpaclab.github.io/Consent-Forms/Active/Consent.html',
+            cont_btn: 'consent-button'
+        }
+
+        let instructionsAndEnterFullscreenTrial = {
+            type: 'fullscreen',
+            message: `
+                <h1>Instructions</h1>
+                <p>Stare at the fixation cross and use your peripheral vision to observe the faces on the left and right.</p>
+                <p>Orient yourself so that you are viewing the screen from 40-50 centimeters away (~2 feet)</p>
+            `
+        }
+
+        let presenceOfIllusionTrial = {
+            type: 'html-keyboard-response',
+            trial_duration: IMAGE_DURATION,
+            choices: jsPsychHandle.NO_KEYS,
+            timeline: [
+                {  
+                    stimulus: function() {
+                        return `
+                        <div class="flashFaceElement">
+                            <img class="flashFaceElement flashFaceImage" src="${jsPsychHandle.timelineVariable('leftFace', true)}"/>
+                            <p class="flashFaceElement flashFaceFixation">+</p>
+                            <img class="flashFaceElement flashFaceImage" src="${jsPsychHandle.timelineVariable('rightFace', true)}"/>
+                        </div>`;
+                    }
+                }
+            ],
+            timeline_variables: function() {
+                var faces = ImageNamesToImages(faceNames);
+                var trialVariables = [];
+                for(var i = 0; i < faces.length; i+=2) {
+                    trialVariables.push({
+                        leftFace: faces[i],
+                        rightFace: faces[i+1]
+                    });
+                }
+                return [].concat(trialVariables, trialVariables, trialVariables);
+            }()
+        }
+
+        let measureDistortionTrial = {
+            type: 'html-slider-response',
+            start: 1,
+            min: 1,
+            max: 7,
+            labels: ['1', '2', '3', '4', '5', '6', '7'],
+            button_label: 'Submit',
+            stimulus: `
+                <p>Rate the amount of distortion seen on the last set of faces</p>
+            `
+        }
+
+        let experimentTrial = {
+            type: 'html-keyboard-response',
+            trial_duration: IMAGE_DURATION,
+            choices: jsPsychHandle.NO_KEYS,
+            timeline: [
+                {  
+                    stimulus: function() {
+                        return `
+                        <div class="flashFaceElement">
+                            <img class="flashFaceElement flashFaceImage" src="${jsPsychHandle.timelineVariable('leftFace', true)}"/>
+                            <p class="flashFaceElement flashFaceFixation">+</p>
+                            <img class="flashFaceElement flashFaceImage" src="${jsPsychHandle.timelineVariable('rightFace', true)}"/>
+                        </div>`;
+                    }
+                }
+            ],
+            timeline_variables: function() {
+                const facesRequired = TRIALS_1*2;
+                var faceNames = jsPsychHandle.randomization.sampleWithoutReplacement(FACE_NAMES, facesRequired);
+
+                var faces = ImageNamesToImages(faceNames);
+                var trialVariables = [];
+                for(var i = 0; i < faces.length; i+=2) {
+                    trialVariables.push({
+                        leftFace: faces[i],
+                        rightFace: faces[i+1]
+                    });
+                }
+                return trialVariables;
+            }()
+        }
+
+        let exitFullscreenTrial = {
+            type: 'fullscreen',
+            fullscreen_mode: false
         }
 
         let finalTrial = {
             type: 'instructions',
-            pages: [`Thanks for participating! Please email us at ${CONTACT_EMAIL}. Push the right arrow key to recieve credit.`]
+            pages: ['Thanks for particpating! Please email us at fake@emial.com.'],
+            allow_keys: false
         }
 
         // Configure and Start Experiment
         jsPsychHandle.init({
-            timeline: [welcomeTrial, finalTrial],
+            timeline: [welcomeTrial, checkVisionTrial, consentFormTrial, instructionsAndEnterFullscreenTrial, presenceOfIllusionTrial, measureDistortionTrial, experimentTrial, exitFullscreenTrial, measureDistortionTrial, finalTrial],
             on_trial_finish: session.insert,
-            on_finish: function() { window.top.location.href = CREDIT_URL }
-        })
+            on_finish: function() { window.top.location.href = CREDIT_URL; }
+        });
     }
 }
---
+++ GenerateAverageFaces.js
@@ -0,0 +1,119 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const SUBJECT_COUNT = 30;
+const FACE_TYPES = ['AAF', 'AAM', 'AF', 'AM', 'LF', 'LM', 'WF', 'WM'];
+const FACE_PER_TYPE = 40;
+const MIN_FACE = 1;
+
+function GenerateRun(min, max)
+{
+    let array = []
+    for (let i = min; i <= max; i++)
+        array.push(i);
+    return array;
+}
+
+function Shuffle(array)
+{
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex)
+    {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
+function CreateConfig(faceMin, faceMax, faceTypes)
+{
+    let config = {};
+    for (let type of faceTypes)
+    {
+        let fullSet = GenerateRun(faceMin, faceMax);
+        fullSet = Shuffle(fullSet);
+        config[type] = {
+            "testSet": fullSet.splice(0, fullSet.length/2),
+            "realSet": fullSet
+        }
+    }
+    return config;
+}
+
+function CreateSubjectConfigs(subjectCount)
+{
+    let subjectConfigs = [];
+    for (let i = 0; i < subjectCount; i++)
+    {
+        subjectConfigs.push(CreateConfig(MIN_FACE, FACE_PER_TYPE, FACE_TYPES));
+    }
+    return subjectConfigs;
+}
+
+function SaveConfigs(configs)
+{
+    fs.writeFile('SubjectConfigs.cfg', `let subjects='${JSON.stringify(configs)}'`, (error) => {
+        if (error !== null)
+            console.error(error);
+    });
+}
+
+function Copy(source, destination, type, images)
+{
+    for (let image of images)
+    {
+        fs.copyFileSync(`${source}/${type}_${image}.jpg`, `${destination}/${type}_${image}.jpg`);
+    }
+}
+
+function Delete(directory, type, images)
+{
+    for (let image of images)
+    {
+        fs.unlinkSync(`${directory}/${type}_${image}.jpg`);
+    }
+}
+
+function Worker(subject, type, folder)
+{
+    let resultName = type;
+    subject[type].realSet.forEach(element => resultName += `_${element}`);
+
+    let command = `python3 facemorpher/averager.py --images=${folder}/ --out=out_folder/${resultName}.png --background=transparent`;
+
+    return new Promise((resolve, reject) =>
+    {
+        console.log(resultName);
+        Copy('examples', folder, type, subject[type].realSet);
+        exec(command, (error, stdout, stderr) => {
+        Delete(folder, type, subject[type].realSet);
+        resolve();
+        });
+    });
+}
+
+async function main()
+{
+    let configs = CreateSubjectConfigs(SUBJECT_COUNT);
+    SaveConfigs(configs);
+    for (let subject of configs)
+    {
+        let workerList = [];
+        for (let type in subject)
+        {
+            workerList.push(Worker(subject, type, type));
+        }
+        for (let worker of workerList)
+        {
+            await worker;
+        }
+    }
+}
+
+main();

ba3c35d54033c0b7d14a4c3433a39e807a1d9787
Grey Johnson <grey.johnson@ufl.edu>
2021-05-03 12:14:43 -0400

    debug mode for review

--- jsSheet/jsSheet.js
+++ jsSheet/jsSheet.js
@@ -1,6 +1,5 @@
 const jsSheet = {
   CreateSession: function(onSuccess, onFailure = console.error) {
-    if (typeof(google) == "undefined") {
       console.error("\"google\" not defined: running in debug mode")
       onSuccess({
         id: -1,
@@ -36,23 +35,5 @@ const jsSheet = {
           console.log(url);
         }
       })
-    }
-
-    google.script.run.withFailureHandler(onFailure).withSuccessHandler((id) => {
-      google.script.run.withFailureHandler(onFailure).withSuccessHandler((subject_number) => {
-        onSuccess({
-          id: id,
-          subject_number: subject_number,
-          insert: function(data) {
-            google.script.run.withFailureHandler(onFailure).Insert(id, subject_number, data)
-          },
-          complete: function(redirectUrl) {
-            google.script.run.withFailureHandler(onFailure).withSuccessHandler(() => {
-              window.top.location.href = redirectUrl
-            }).CompleteSubject(subject_number)
-          }
-        })
-      }).GetSubject()
-    }).GetSessionID()
   }
 }
